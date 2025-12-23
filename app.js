77290117bac423250a804462e64b5bd5d5df9ffc
sua_sii <suengmin64@gmail.com>
2025-12-23 12:24:32 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,85 @@
+//===== chapter 1 Q1 =====//
+alert ('welcome to my website');
+//===== Q2 =====//
+alert ('Error! please enter a valid password');
+//===== Q3 =====//
+alert('Welcome to JS land...\n Happy  Coding!');
+//===== Q4 =====//
+alert('welcome to Js land');
+alert('Happy Coding!');
+//===== Q5 =====//
+console.log('Hello... I can run JS through my web browser console')
+alert('Hello... I can run JS through my web browser console');
+//===== Q6 skip =====//
+
+//===== Q7 skip =====//
+
+ //===== chapter 2 Q1 =====// 
+  var username;
+  //===== Q2 =====//
+ var myname="fajar nadeem";
+ //===== Q3 =====//
+  var message
+ message="hello world"
+  alert(message);
+  //===== Q4 =====//
+  var studentname="Fajar"
+ var age="15"
+  var course="web development"
+ alert(studentname)
+ alert(age)
+ alert(course)
+  //===== Q5 =====//
+  var pizza="pizza\n pizz\n piz\n pi\n p"
+  alert(pizza)
+  //===== Q6 =====//
+var email="fajar@gmail.com"
+alert('my email address is ' + email)
+ //===== Q7 =====//
+ var book="A smarter way to learn JavaScript"
+ alert('I am trying to learn from the book '+ book)
+  //===== Q8 =====//
+  document.write('Yah! I can write HTML content through JavaScript')
+   //===== Q9 =====//
+   var ending=" ▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬  "
+   alert(ending)
+   document.write(ending)
+
+ //===== chapter 3 Q1 =====//
+ var age="15"
+ alert(age)
+ //===== Q2 =====//
+ var visitCount = 13;
+document.write("You have visited this site" + visitCount + "times.<br>");
+//===== Q3 =====//
+var birthyear=1227
+document.write("my birthyear is "+birthyear+"<br> Datatype of my declared variable is number <br>")
+//===== Q4 =====//
+var visitorName = "Fajar"
+var productTitle = "T-shirts"
+var quantity = 5
+document.write(visitorName +  "ordered" +quantity  + " "  +productTitle  + "(s) on XYZ Clothing store.");
+//===== chapter 4 Q1 =====//
+var firstname, lastname, email;  
+//===== Q2 =====//
+//===== LEGAL =====//
+var studentname;
+var name$;
+var LastName;
+var username99;
+var first_name;
+
+//===== ILLEGAL =====//
+//var student name;
+//var !username;
+//var Last-Name;
+//var 1username;
+//var @myname;
+ 
+
+//===== Q3 =====//
+ document.write("<h1>Rules for naming JS variables</h1>")
+ document.write("Variable names can only contain letter, digits, $ or _. Example : $my_1st variable <br>")
+ document.write(" Variables must begin with a letter,$ or _. Example: $name,_name or name<br>")
+ document.write("Variable names are case sensitive <br>")
+ document.write(" Variable names should not be JS keywords");
